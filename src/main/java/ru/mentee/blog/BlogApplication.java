8c009ba9c1d4683d114cdf72ef713a7af8ee5ee6
pavelKomiss121 <pavelkomiss@gmail.com>
2026-01-10 17:31:02 +0300

    создание приложения

---
+++ src/main/java/ru/mentee/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package ru.mentee.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+  public static void main(String[] args) {
+    // Устанавливаем профиль "blog" программно
+    System.setProperty("spring.profiles.active", "blog");
+    SpringApplication.run(BlogApplication.class, args);
+  }
+}
