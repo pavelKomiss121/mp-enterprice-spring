022eb8e44399c1cc3a1314f1b774851fab41cf5b
pavelKomiss121 <pavelkomiss@gmail.com>
2026-01-10 17:33:08 +0300

    резолвер

---
+++ src/main/java/ru/mentee/blog/api/resolver/CurrentUserArgumentResolver.java
@@ -0,0 +1,37 @@
+package ru.mentee.blog.api.resolver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import ru.mentee.blog.api.annotation.CurrentUser;
+import ru.mentee.blog.domain.model.User;
+
+@Component
+@Slf4j
+public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+
+  @Override
+  public boolean supportsParameter(MethodParameter parameter) {
+    return parameter.hasParameterAnnotation(CurrentUser.class);
+  }
+
+  @Override
+  public Object resolveArgument(
+      MethodParameter parameter,
+      ModelAndViewContainer mavContainer,
+      NativeWebRequest webRequest,
+      WebDataBinderFactory binderFactory) {
+
+    log.debug("Resolving @CurrentUser argument");
+
+    return User.builder()
+        .username("demo")
+        .email("demo@example.com")
+        .build();
+  }
+}
